53a045d81df615d30df458cd29bab69a8adb21c0
Nicolás Santángelo <santangelonicolas@gmail.com>
2023-04-28 03:08:12 -0300

    mobile

--- main.js
+++ main.js
@@ -7,11 +7,19 @@
 
   function openBox() {
     boxBody.classList.add("box-body-hovered");
+    hiddenContent.style.display = "block";
 
     setTimeout(function () {
       hiddenContent.classList.add("visible");
+
+      const mobileElements = document.querySelectorAll(".xs-full-height, .xs-overflow-hidden");
+
+      for (const element of mobileElements) {
+        element.classList.remove("xs-full-height");
+        element.classList.remove("xs-overflow-hidden");
+      }
     }, 2000);
 
-    box.removeEventListener(openBox);
+    box.removeEventListener("click", openBox);
   }
 })();
